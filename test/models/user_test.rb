82860327c0ef1854bfafa060b8bc1e193576ab55
gregurton <gburt802@gmail.com>
2014-10-12 23:18:27 -0400

    took a crack at a few user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def check_field_is_required
+
+  test "user's name is required when signing up"
+    check_field_is_required(:name, "Frank")
+  end
+
+  test "email address is required when signing up"
+    check_field_is_required(:email, "frank@frank.com")
+  end
+
+  test "password is required when signing up"
+    check_field_is_required(:password, "frankspassword")
+  end
+
   # test "the truth" do
   #   assert true
   # end
