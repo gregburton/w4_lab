237a2f9a40a0197aeb124733fbab8defd65e91f3
gregurton <gburt802@gmail.com>
2014-10-12 23:20:17 -0400

    quick amendment

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,16 +3,18 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def check_field_is_required
 
-  test "user's name is required when signing up"
+  end
+
+  test "user's name is required when signing up" do
     check_field_is_required(:name, "Frank")
   end
 
-  test "email address is required when signing up"
+  test "email address is required when signing up" do
     check_field_is_required(:email, "frank@frank.com")
   end
 
-  test "password is required when signing up"
-    check_field_is_required(:password, "frankspassword")
+  test "password is required when signing up" do
+    check_field_is_required(:password, "franksredhotsauce")
   end
 
   # test "the truth" do
